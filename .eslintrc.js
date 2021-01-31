3782640855056441d5eca43c96c5ecd1de45b11c
Yemshyn Vladimir <50299883+Episodel@users.noreply.github.com>
2021-01-31 05:36:40 +0200

    Update .eslintrc.js
    rules fix 1.0.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,36 +2,40 @@ module.exports = {
   root: true,
   overrides: [
     {
-      files: ["*.ts"],
+      files: ['*.ts'],
       parserOptions: {
-        project: [
-          "tsconfig.*?.json",
-          "e2e/tsconfig.json"
-        ],
-        createDefaultProgram: true
+        project: ['tsconfig.*?.json', 'e2e/tsconfig.json'],
+        createDefaultProgram: true,
       },
       extends: [
-        "plugin:@angular-eslint/recommended",
+        'plugin:@angular-eslint/recommended',
         // Стайл гайд AirBnB
         'airbnb-typescript/base',
         // Настройки для Prettier
         'prettier/@typescript-eslint',
-        'plugin:prettier/recommended'
+        'plugin:prettier/recommended',
       ],
       rules: {
-        ...
-      }
+        'import/prefer-default-export': 'off',
+        'no-useless-constructor': 'off',
+        '@angular-eslint/no-empty-lifecycle-method': 'off',
+        '@typescript-eslint/no-useless-constructor': 'off',
+        'class-methods-use-this': 'off',
+        'no-empty-function': ['error', { allow: ['constructors', 'functions'] }],
+      },
     },
+
     {
-      files: ["*.component.html"],
-      extends: ["plugin:@angular-eslint/template/recommended"],
+      files: ['*.component.html'],
+      extends: ['plugin:@angular-eslint/template/recommended'],
       rules: {
-        "max-len": ["error", { "code": 140 }]
-      }
+        'max-len': ['error', { code: 120 }],
+      },
     },
     {
-      files: ["*.component.ts"],
-      extends: ["plugin:@angular-eslint/template/process-inline-templates"]
-    }
-  ]
-}
+      files: ['*.component.ts'],
+      extends: ['plugin:@angular-eslint/template/process-inline-templates'],
+    },
+  ],
+};
+
